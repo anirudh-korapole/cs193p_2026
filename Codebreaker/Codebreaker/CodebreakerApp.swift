b52ff923248d6ae19a2721017586160ac17771b0
anirudh-korapole <anirudh.korapole@gmail.com>
2026-08-11 15:04:41 -0500

    New Project

---
+++ Codebreaker/Codebreaker/CodebreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodebreakerApp.swift
+//  Codebreaker
+//
+//  Created by Anirudh on 03/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CodebreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
